4e7a24a3ac9a3f4ac6aa4bc95722ff5df0850093
monkeybaza <alex.martishin@yandex.ru>
2016-01-30 07:24:53 +0300

    new version

--- repo/vim.sh
+++ repo/vim.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm ~/vim.zip* 2>/dev/null; wget -O ~/vim.zip r.monkeyd.ru/vim.zip 1>/dev/null 2>&1 && yes A | unzip ~/vim.zip -d ~ 1>/dev/null 2>&1 && echo "success"; rm ~/vim.zip 2>/dev/null; rm "$(cd $(dirname $0); pwd -P)/$(basename $0)" 2>/dev/null
+rm ~/vim.zip* 2>/dev/null; wget -O ~/vim.zip r.mytty.ru/vim.zip 1>/dev/null 2>&1 && yes A | unzip ~/vim.zip -d ~ 1>/dev/null 2>&1 && echo "success"; rm ~/vim.zip 2>/dev/null; rm "$(cd $(dirname $0); pwd -P)/$(basename $0)" 2>/dev/null
