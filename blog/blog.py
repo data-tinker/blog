3bf105526a4f64caabde00c696d5c4bf9f98b6c2
monkeybaza <alex.martishin@yandex.ru>
2016-02-10 22:31:22 +0300

    added timestamp coversion

--- blog/blog.py
+++ blog/blog.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template, jsonify
 from datetime import date
+from datetime import datetime
 from urlparse import urlparse, urlunparse
 
 app = Flask(__name__)
@@ -16,6 +17,16 @@ def index():
 @app.route('/tutorial')
 def tutorial():
     return redirect('http://178.79.163.43:5000', code=302)
+
+@app.route('/timestamp')
+def timestamp_render():
+    if request.args.get('value'):
+        timestamp = int(request.args.get('value'))
+        if len(str(timestamp)) == 13:
+                timestamp = int(str(timestamp)[0:-3])
+        result = str(datetime.fromtimestamp(timestamp))
+        return render_template('timestamp.html', timestamp=timestamp, result=result)
+    return render_template('timestamp.html')
 
 @app.route('/static/<path:path>')
 def send_static():
